e40e79ae2a30dcd6559abf9390b512810c42c4ca
fujii daigo <daigofujii1116@gmail.com>
2025-02-13 10:15:17 +0900

    firstBuild_0213

---
+++ src/database/migrations/2025_02_13_094907_create_merchandise_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMerchandiseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('merchandise', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->integer('price');
+            $table->string('discription', 100);
+            $table->string('img_url', 255);
+            $table->string('condition', 100);
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('merchandise');
+    }
+}
