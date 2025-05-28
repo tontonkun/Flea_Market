bcc978122355280fbed98861068af71afe04f3f0
tontonkun <daigofujii1116@gmail.com>
2025-05-28 16:51:55 +0900

    0528_update

--- src/config/app.php
+++ src/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://flea-market-hyai.onrender.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
